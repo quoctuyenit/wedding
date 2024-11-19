78af0348a1db68993b950cd9bd7b6116c3fc8a5e
Trần Quốc Tuyền <tranquoctuyen4995@gmail.com>
2024-11-20 01:15:57 +0700

    update path data.json

--- js/init.js
+++ js/init.js
@@ -1,5 +1,5 @@
 import '../common/emoji-picker/js/index.js'
-$(".textarea-emoji-picker emoji-picker").dataSource = '../common/emoji-picker/js/data.json';
+$(".textarea-emoji-picker emoji-picker").dataSource = './common/emoji-picker/js/data.json';
 $(document).ready(function() {
     $(document).on('emoji-click', '.textarea-emoji-picker emoji-picker', function(e){
         let formInput = $(this).parents('.textarea-emoji-picker').find("textarea");
